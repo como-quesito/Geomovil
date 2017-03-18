f76a6a4e41615da4ab8830d50d2888ce2fd8018e
como-quesito <rapidclimate@outlook.com>
2017-03-18 07:04:48 -0600

    Initial commit

---
+++ app/src/main/java/unitec/org/geomovil/TareaAutenticarse.java
@@ -0,0 +1,26 @@
+package unitec.org.geomovil;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+
+public class TareaAutenticarse extends AsyncTask<Integer, String, String> {
+
+    public  TareaAutenticarse(Context ctx){
+
+    }
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(Integer... integers) {
+        return null;
+    }
+}
